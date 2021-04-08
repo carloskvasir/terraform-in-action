d8dcb080750b11fc24dbab19aa83cf0f5a0e2152
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-04-08 08:58:00 -0300

    chore: version of provider

--- version.tf
+++ version.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 0.14, < 0.15"
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
+      version = ">= 2, <3"
     }
   }
 }
