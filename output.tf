31663e6bf153cd4456b08475de7506c83c51a0e6
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-04-22 23:04:42 -0300

    chore: update terraform version

--- output.tf
+++ output.tf
@@ -8,8 +8,10 @@ output "load_balancer_ip" {
 
 output "database_password" {
   value = digitalocean_database_user.username.password
+  sensitive = true
 }
 
 output "database_uri" {
   value = digitalocean_database_cluster.postgres.uri
+  sensitive = true
 }
